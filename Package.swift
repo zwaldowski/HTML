e0a35086e538de696fe27c9eab1534ed68b7d714
Zachary Waldowski <zach@waldowski.me>
2023-09-08 16:33:13 -0400

    Drop old code

--- Package.swift
+++ Package.swift
@@ -7,29 +7,14 @@ let package = Package(
     platforms: [ .iOS(.v15), .macCatalyst(.v15), .macOS(.v12), .tvOS(.v15) ],
     products: [
         .library(
-            name: "HTMLTree",
-            targets: [ "HTMLTree" ]),
-        .library(
             name: "HTMLAttributedString",
             targets: [ "HTMLAttributedString" ]),
     ],
     targets: [
         .target(
-            name: "HTMLTree",
-            dependencies: [
-                .target(name: "libxml2", condition: .when(platforms: [ .linux, .android ]))
-            ]),
-        .testTarget(
-            name: "HTMLTreeTests",
-            dependencies: [ "HTMLTree" ]),
-        .target(
             name: "HTMLAttributedString"),
         .testTarget(
             name: "HTMLAttributedStringTests",
-            dependencies: [ "HTMLAttributedString" ]),
-        .systemLibrary(
-            name: "libxml2",
-            pkgConfig: "libxml-2.0",
-            providers: [ .apt([ "libxml2-dev" ]) ])
+            dependencies: [ "HTMLAttributedString" ])
     ]
 )
